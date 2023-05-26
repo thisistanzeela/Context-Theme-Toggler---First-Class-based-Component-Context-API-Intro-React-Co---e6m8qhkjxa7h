9fce8b2fd7aaef8c6c88a7ca74f5cd6ee9463237
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-05-27 00:54:20 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,40 @@
-import React from 'react';
+// import React from 'react';
+// import { LocalThemedBox } from './LocalThemedBox';
+// import { ThemeContext } from './ThemeProvider';
+
+// const Page = () => {
+
+//     return(
+//         <div className={"container"} id="themed-page">
+//             <p id="themed-text-container">
+//                 lorem ipsum dolor iterit n stuff
+//             </p>
+//             <button className="btn" id="themed-button">Themed Button</button>
+//             <LocalThemedBox />
+//         </div>
+//     )
+// }
+
+// export { Page }
+
+import React, { useContext } from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Page = () => {
+  const { theme } = useContext(ThemeContext);
 
-    return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
-                lorem ipsum dolor iterit n stuff
-            </p>
-            <button className="btn" id="themed-button">Themed Button</button>
-            <LocalThemedBox />
-        </div>
-    )
-}
+  return (
+    <div className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`} id="themed-page">
+      <p id="themed-text-container" className={`txt-${theme}`}>
+        lorem ipsum dolor iterit n stuff
+      </p>
+      <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">
+        Themed Button
+      </button>
+      <LocalThemedBox />
+    </div>
+  );
+};
 
-export { Page }
+export { Page };
