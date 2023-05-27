82d300637ba4b3765fa77ff8cad247b91e74748b
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-05-27 15:22:39 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -25,8 +25,8 @@ const Page = () => {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`} id="themed-page">
-      <p id="themed-text-container" className={`txt-${theme}`}>
+    <div className={`container bg-${theme}`} id="themed-page">
+      <p className={`txt-${theme}`} id="themed-text-container">
         lorem ipsum dolor iterit n stuff
       </p>
       <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">
