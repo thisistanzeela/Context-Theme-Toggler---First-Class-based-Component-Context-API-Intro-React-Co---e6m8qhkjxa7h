c35cf288a41e7a4fe9671ffb722e4e79a0b55639
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-05-27 15:37:29 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -12,21 +12,80 @@
 
 // export {ThemeProvider,ThemeContext}
 
-import React, { useState } from 'react';
-import { ThemeContext } from './ThemeProvider';
 
-const ThemeProvider = (props) => {
-  const [theme, setTheme] = useState('light');
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { ThemeProvider } from './ThemeProvider';
+import { Page } from './Page';
+import { LocalThemedBox } from './LocalThemedBox';
+import { ThemeToggleButton } from './ThemeToggleButton';
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
+describe('ThemeProvider', () => {
+  test('All elements initially follow the light theme', () => {
+    const { getByTestId } = render(
+      <ThemeProvider>
+        <Page />
+        <ThemeToggleButton />
+        <LocalThemedBox />
+      </ThemeProvider>
+    );
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {props.children}
-    </ThemeContext.Provider>
-  );
-};
+    expect(getByTestId('themed-page')).toHaveClass('bg-light');
+    expect(getByTestId('themed-text-container')).toHaveClass('txt-light');
+    expect(getByTestId('themed-button')).toHaveClass('btn-light', 'txt-light');
+    expect(getByTestId('local-themed-box')).toHaveClass('bg-light');
+    expect(getByTestId('local-themed-text-container')).toHaveClass('txt-light');
+    expect(getByTestId('local-theme-toggler')).toHaveClass('btn-light');
+    expect(getByTestId('global-theme-toggler')).toHaveClass(
+      'btn-light',
+      'txt-light'
+    );
+  });
 
+  test('On clicking the global theme toggle button, all elements change to dark classes', () => {
+    const { getByTestId } = render(
+      <ThemeProvider>
+        <Page />
+        <ThemeToggleButton />
+        <LocalThemedBox />
+      </ThemeProvider>
+    );
+
+    fireEvent.click(getByTestId('global-theme-toggler'));
+
+    expect(getByTestId('themed-page')).toHaveClass('bg-dark');
+    expect(getByTestId('themed-text-container')).toHaveClass('txt-dark');
+    expect(getByTestId('themed-button')).toHaveClass('btn-dark', 'txt-dark');
+    expect(getByTestId('local-themed-box')).toHaveClass('bg-dark');
+    expect(getByTestId('local-themed-text-container')).toHaveClass('txt-dark');
+    expect(getByTestId('local-theme-toggler')).toHaveClass('btn-dark');
+    expect(getByTestId('global-theme-toggler')).toHaveClass(
+      'btn-dark',
+      'txt-dark'
+    );
+  });
+
+  test('On clicking the local theme toggler button, only the locally themed box element changes', () => {
+    const { getByTestId } = render(
+      <ThemeProvider>
+        <Page />
+        <ThemeToggleButton />
+        <LocalThemedBox />
+      </ThemeProvider>
+    );
+
+    fireEvent.click(getByTestId('local-theme-toggler'));
+
+    expect(getByTestId('themed-page')).toHaveClass('bg-light');
+    expect(getByTestId('themed-text-container')).toHaveClass('txt-light');
+    expect(getByTestId('themed-button')).toHaveClass('btn-light', 'txt-light');
+    expect(getByTestId('local-themed-box')).toHaveClass('bg-dark');
+    expect(getByTestId('local-themed-text-container')).toHaveClass('txt-light');
+    expect(getByTestId('local-theme-toggler')).toHaveClass('btn-dark');
+    expect(getByTestId('global-theme-toggler')).toHaveClass(
+      'btn-light',
+      'txt-light'
+    );
+  });
+});
 export { ThemeProvider };
