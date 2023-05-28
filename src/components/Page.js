136ee906c04de83077baba01d014e8aaadf6dfeb
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-05-28 21:30:30 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -16,24 +16,27 @@
 // }
 
 // export { Page }
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
-import { ThemeContext } from './ThemeProvider';
+import { ThemeContext, ThemeProvider } from './ThemeProvider';
 
 const Page = () => {
-  const { theme } = useContext(ThemeContext);
 
-  return (
-    <div className={`container bg-${theme}`} id="themed-page">
-      <p className={`txt-${theme}`} id="themed-text-container">
-        lorem ipsum dolor iterit n stuff
-      </p>
-      <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">
-        Themed Button
-      </button>
-      <LocalThemedBox />
-    </div>
-  );
-};
+    const {theme,setTheme} = useContext(ThemeContext);
 
+    return(
+        <div className={'bg-'+theme} id="themed-page">
+            <p id="themed-text-container" className={'txt-'+theme}>
+                lorem ipsum dolor iterit n stuff
+            </p>
+            {/* 'btn btn-'+theme 'txt-'+theme */}
+            <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">Themed Button</button>
+           
+        <LocalThemedBox/>
+           
+        </div>
+    )
+}
+
+export { Page }
 export { Page };
